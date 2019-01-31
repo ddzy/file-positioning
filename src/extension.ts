c13952a110fc8fa061cc7acb31561f3910004143
yang <1766083035@qq.com>
2019-01-31 16:12:10 +0800

    :sparkles: add keybinds bo run extension

--- src/extension.ts
+++ src/extension.ts
@@ -5,11 +5,23 @@ export function activate(context: vscode.ExtensionContext) {
 
 	console.log('Congratulations, your extension "file-positioning" is now active!');
 
-	let disposable = vscode.commands.registerCommand('extension.helloWorld', () => {
-		vscode.window.showInformationMessage('Hello World!');
+	const disposable = vscode.commands.registerCommand('extension.helloWorld', () => {
+
+		vscode.window.showInformationMessage('');
+
+	});
+
+	const disposableTwo = vscode.commands.registerCommand('extension.fileposition', () => {
+
+		const input = vscode.window.createInputBox();
+		vscode.window.showInputBox(input).then((v) => {
+			vscode.window.showInformationMessage(`${v}`);
+		});
+
 	});
 
 	context.subscriptions.push(disposable);
+	context.subscriptions.push(disposableTwo);
 }
 
 
